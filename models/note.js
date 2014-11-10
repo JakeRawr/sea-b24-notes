7c5961b78da7710bc2b308b2f49f6f4bad3a3a26
Jake Yang <tsunghan614@gmail.com>
2014-11-10 10:24:36 -0800

    heroku

--- models/note.js
+++ models/note.js
@@ -1,7 +1,12 @@
+'use strict';
 var mongoose = require('mongoose');
 
+function validator (v) {
+  return v.length >= 4;
+}
+
 var noteSchema = mongoose.Schema({
-  noteBody: 'String'
+  noteBody: {type: String, validate: [validator, 'noteBody value has to be greater than 3'] }
 });
 
 module.exports = mongoose.model('Note', noteSchema);
--- server.js
+++ server.js
@@ -1,11 +1,42 @@
+'use strict';
+
 var express = require('express');
 var mongoose = require('mongoose');
 var bodyparser = require('body-parser');
 var app = express();
-
 app.use(bodyparser.json());
 
-mongoose.connect(process.env.MONGO_URL || 'mongodb://localhost/notes_development');
+var mongoose = require('mongoose');
+var uriUtil = require('mongodb-uri');
+
+/*
+ * Mongoose by default sets the auto_reconnect option to true.
+ * We recommend setting socket options at both the server and replica set level.
+ * We recommend a 30 second connection timeout because it allows for
+ * plenty of time in most operating environments.
+ */
+var options = { server: { socketOptions: { keepAlive: 1, connectTimeoutMS: 30000 } },
+                replset: { socketOptions: { keepAlive: 1, connectTimeoutMS : 30000 } } };
+
+/*
+ * Mongoose uses a different connection string format than MongoDB's standard.
+ * Use the mongodb-uri library to help you convert from the standard format to
+ * Mongoose's format.
+ */
+var mongodbUri = 'mongodb://heroku_app31476844:f09qkl214v2rvf1fcgvocifcqt@ds051980.mongolab.com:51980/heroku_app31476844';
+var mongooseUri = uriUtil.formatMongoose(mongodbUri);
+
+mongoose.connect(mongooseUri, options);
+
+var db = mongoose.connection;
+
+db.on('error', console.error.bind(console, 'connection error:'));
+
+db.once('open', function callback () {
+  console.log('Database connected');
+});
+
+//mongoose.connect(process.env.MONGO_URL || 'mongodb://localhost/notes_development');
 
 require('./routes/notes_routes')(app);
 
